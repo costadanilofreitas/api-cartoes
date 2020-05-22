5b15da55e9772ab70176543150d99dfb3fc4a235
nivio lima <nivioplima@yahoo.com>
2020-05-22 14:36:31 -0300

    Incluir Cartaoservice

---
+++ src/main/java/com/br/api/cartao/services/CartaoService.java
@@ -0,0 +1,85 @@
+package com.br.api.cartao.services;
+
+
+import com.br.api.cartao.models.Cliente;
+import com.br.api.cartao.repositories.ClienteRepository;
+
+import com.br.api.cartao.models.Cartao;
+import com.br.api.cartao.repositories.CartaoRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CartaoService {
+    @Autowired
+    private CartaoRepository cartaoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Iterable<Cliente> buscarTodosClientes(List<Integer> clientesId){
+        Iterable<Cliente> clienteIterable = clienteRepository.findAllById(clientesId);
+        return clienteIterable;
+    }
+
+
+    public Cartao salvarCartao(Cartao cartao){
+        Optional<Cliente> clienteOptional = clienteRepository.findById(cartao.getIdCliente());
+        if (clienteOptional.isPresent()){
+            Cartao cartaoObjeto = cartaoRepository.save(cartao);
+            return cartaoObjeto;
+        }
+        throw new org.hibernate.ObjectNotFoundException(Cartao.class, "Código de Cliente Inexistente!");
+    }
+
+    public Optional<Cartao> buscarPorId(int id){
+        Optional<Cartao> cartaoOptional = cartaoRepository.findById(Long(id));
+        return cartaoOptional;
+    };
+
+    public Iterable<Cartao> buscarTodosCartoes(){
+        Iterable<Cartao> cartoes = cartaoRepository.findAll();
+        return cartoes;
+    }
+
+    public Cartao atualizarCartao(Cartao cartao) throws ObjectNotFoundException{
+        Optional<Cartao> cartaoOptional = buscarPorId(cartao.getId());
+        if (cartaoOptional.isPresent()){
+            Cartao cartaoData = cartaoOptional.get();
+            if (cartao.getCvv() == 0){
+                cartao.setCvv(cartaoData.getCvv());
+            }
+            if (cartao.getLimiteAtual() == 0.0){
+                cartao.setLimiteAtual(cartaoData.getLimiteAtual());
+            }
+            if (cartao.getValidade() == null){
+                cartao.setValidade(cartaoData.getValidade());
+            }
+            if (cartao.getLimiteTotal() == 0.0){
+                cartao.setLimiteTotal(cartaoData.getLimiteTotal());
+            }
+            if (cartao.getIdCliente() == 0){
+                cartao.setIdCliente(cartaoData.getIdCliente());
+                Cartao cartaoObjeto = cartaoRepository.save(cartao);
+                return cartaoObjeto;
+            }else{
+                Optional<Cliente> clienteOptional = clienteRepository.findById(cartao.getIdCliente());
+                if (clienteOptional.isPresent()){
+                    Cartao cartaoObjeto = cartaoRepository.save(cartao);
+                    return cartaoObjeto;
+                }
+                throw new org.hibernate.ObjectNotFoundException(Cartao.class, "Novo Código de Cliente Inexistente!");
+            }
+        }
+        throw new org.hibernate.ObjectNotFoundException(Cartao.class, "O Cartão não foi encontrado");
+    }
+
+
+    public void deletarCartao(Cartao cartao){
+        cartaoRepository.delete(cartao);
+    }
+}
