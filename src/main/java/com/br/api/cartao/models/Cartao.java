3092a7a1ad20978291936cfbffe8a48e327f485d
a2 <a2@localhost.localdomain>
2020-05-22 13:55:23 -0300

    Incluir Validações no Modelo do Cartao

--- src/main/java/com/br/api/cartao/models/Cartao.java
+++ src/main/java/com/br/api/cartao/models/Cartao.java
@@ -1,16 +1,31 @@
 package com.br.api.cartao.models;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
 public class Cartao {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long numeroCartao;
+    @NotNull
     private double limiteTotal;
+    @DateTimeFormat
     private Date validade;
+    @NotNull
+    @Size(max = 3, message = "Cvv é obrigatorio informar três digitos")
     private int cvv;
+    @NotNull
     private double limiteAtual;
+    @NotNull
     private int idCliente;
 
     public Cartao() {
