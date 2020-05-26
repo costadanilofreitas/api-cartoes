9c3e09ca30aff3f0b6d3277c1230e0b0c830dbb2
maiacir <carolinamaia187@gmail.com>
2020-05-26 12:03:03 -0300

    Cliente Service e TDD

---
+++ src/main/java/com/br/api/cartao/services/ClienteService.java
@@ -0,0 +1,45 @@
+package com.br.api.cartao.services;
+
+import com.br.api.cartao.models.Cliente;
+import com.br.api.cartao.repositories.ClienteRepository;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente salvarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Optional<Cliente> buscarPorId (int id) {
+        Optional<Cliente> clienteOptional = clienteRepository.findById(id);
+        return clienteOptional;
+    }
+
+    public Iterable<Cliente> buscarTodosClientes() {
+        Iterable<Cliente> clientes = clienteRepository.findAll();
+        return clientes;
+    }
+
+    public Cliente atualizarCliente(Cliente cliente) throws ObjectNotFoundException {
+        Optional<Cliente> clienteOptional = buscarPorId(cliente.getId());
+        if (clienteOptional.isPresent()) {
+            Cliente clienteObjeto = clienteRepository.save(cliente);
+            return clienteObjeto;
+        }
+        throw new ObjectNotFoundException(Cliente.class, "O cliente não foi encontrado");
+    }
+
+    public void deletarCliente(Cliente cliente) {
+        clienteRepository.delete(cliente);
+    }
+
+
+}
