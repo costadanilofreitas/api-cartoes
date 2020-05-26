00879c8ffc3792d411e17663368f73ba52857357
Jonathan Roberto <jonathan.pontes@hotmail.com>
2020-05-26 17:32:37 -0300

    Correção de classe cliente

--- src/main/java/com/br/api/cartao/models/Cliente.java
+++ src/main/java/com/br/api/cartao/models/Cliente.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
@@ -18,22 +17,25 @@ public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
+
     @NotBlank
     @Size(min=8, max=100, message = "Nome do cliente deve ter entre 8 a 100 caracteres")
-    @NotNull
     private String nome;
+
     @CPF(message = "CPF Inválido")
     private String cpf;
-    @DateTimeFormat
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date dataNascimento;
+
     @Email(message = "O formato do e-mail é inválido")
     private String email;
 
     public Cliente() {
     }
 
-    public Cliente(Integer id, @NotBlank @Size(min = 8, max = 100, message = "Nome do cliente deve ter entre 8 a 100 caracteres") @NotNull String nome, @CPF(message = "CPF Inválido") String cpf, Date dataNascimento, @Email(message = "O formato do e-mail é inválido") String email) {
+    public Cliente(int id, String nome, String cpf, Date dataNascimento, String email) {
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
